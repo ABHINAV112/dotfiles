d9fec287b66aa07adcbfdc1f1a5e5d6beb9f4ccc
ABHINAV112 <nabhinav2000@gmail.com>
2024-03-03 23:32:44 +0800

    feat: adding neovim-treesitter-context

--- .config/nvim/lua/abhinav/packer.lua
+++ .config/nvim/lua/abhinav/packer.lua
@@ -43,6 +43,8 @@ return require('packer').startup(function(use)
         requires = { 'nvim-tree/nvim-web-devicons', opt = true }
     }
 
+    use("nvim-treesitter/nvim-treesitter-context");
+
     use {
         'VonHeikemen/lsp-zero.nvim',
         branch = 'v2.x',
