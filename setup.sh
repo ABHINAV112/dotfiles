62d22bdca461448a77885b837dd2d878320f3a01
ABHINAV112 <nabhinav2000@gmail.com>
2024-03-22 14:51:28 +0800

    add a script that detects file change

--- setup.sh
+++ setup.sh
@@ -3,11 +3,9 @@ chmod +x $HOME/bin/*
 git config --global push.autoSetupRemote true
 
 brew install zoxide
-
 brew install fzf
-
 brew install ripgrep
-
 brew install gh
+brew install fswatch
 
 tmux source $HOME/.tmux.conf
