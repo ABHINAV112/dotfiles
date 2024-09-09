539dd503d5e213236b0c4525bdba58060f2c5c3a
Abhinav NB <Abhinav_Balasubraman@asus.com>
2024-09-09 14:48:30 +0800

    refactor: avoid highlighting all search terms

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -3,3 +3,13 @@
 -- Add any additional options here
 --
 vim.opt.clipboard = ""
+
+-- avoid highlighting search terms
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+-- long undodir
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
